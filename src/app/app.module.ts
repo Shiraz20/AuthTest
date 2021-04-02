a6dca33e70f245164fb88b8c8eb0264ff93a583f
Shiraz Besrour <50299287+Shiraz20@users.noreply.github.com>
2021-04-02 15:11:18 +0300

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { PublicModule } from './public/public.module';
+import { SecureComponent } from './secure/secure.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SecureComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PublicModule,
+    RouterModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
