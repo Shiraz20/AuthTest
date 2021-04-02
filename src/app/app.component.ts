4a8f46dae97f8e47ccdf0265cfe55d76f5c9d58b
Shiraz Besrour <50299287+Shiraz20@users.noreply.github.com>
2021-04-02 23:16:16 +0300

    commit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent implements OnInit {
 
   loggedIn=false;
+  
   ngOnInit()
   {
       this.loggedIn = localStorage.getItem("token") !== null;
